f06c1ccff4ea61233c4b2e46ed8ae75ea3dc2632
Changsu Im <imcxsu@gmail.com>
2021-04-08 23:02:39 +0900

    add: money-kent-beck ch9

--- money-kent-beck/src/main/java/com/example/Dollar.java
+++ money-kent-beck/src/main/java/com/example/Dollar.java
@@ -1,12 +1,12 @@
 package com.example;
 
 public class Dollar extends Money {
-  Dollar(int amount) {
-    this.amount = amount;
+  Dollar(int amount, String currency) {
+    super(amount, currency);
   }
 
   @Override
   Money times(int multiplier) {
-    return new Dollar(amount * multiplier);
+    return Money.dollar(amount * multiplier);
   }
 }
--- money-kent-beck/src/main/java/com/example/Franc.java
+++ money-kent-beck/src/main/java/com/example/Franc.java
@@ -1,12 +1,12 @@
 package com.example;
 
 public class Franc extends Money {
-  Franc(int amount) {
-    this.amount = amount;
+  Franc(int amount, String currency) {
+    super(amount, currency);
   }
 
   @Override
   Money times(int multiplier) {
-    return new Franc(amount * multiplier);
+    return Money.franc(amount * multiplier);
   }
 }
