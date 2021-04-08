68f76dd888fff493d0466be68777d9cfc6d42d7b
Changsu Im <imcxsu@gmail.com>
2021-04-08 23:20:28 +0900

    add: money-kent-beck ch11

--- money-kent-beck/src/main/java/com/example/Money.java
+++ money-kent-beck/src/main/java/com/example/Money.java
@@ -1,6 +1,6 @@
 package com.example;
 
-abstract class Money {
+class Money {
   protected int amount;
   protected String currency;
 
@@ -9,23 +9,31 @@ abstract class Money {
     this.currency = currency;
   }
 
-  abstract Money times(int multiplier);
+  Money times(int multiplier) {
+    return new Money(amount * multiplier, currency);
+  }
 
   @Override
   public boolean equals(Object object) {
     Money money = (Money) object;
-    return amount == money.amount && getClass().equals(money.getClass());
+    return amount == money.amount && currency().equals(money.currency());
   }
 
-  static Dollar dollar(int amount) {
-    return new Dollar(amount, "USD");
+  @Override
+  public String toString() {
+    return amount + " " + currency;
+  }
+
+  static Money dollar(int amount) {
+    return new Money(amount, "USD");
   }
   
-  static Franc franc(int amount) {
-    return new Franc(amount, "CHF");
+  static Money franc(int amount) {
+    return new Money(amount, "CHF");
   }
 
   String currency() {
     return currency;
   }
+
 }
