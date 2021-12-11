950df6ec74c1b31096c1c0a78160aae14b720678
Changsu Im <imcxsu@gmail.com>
2021-12-11 23:52:13 +0900

    Test query annotation - spring data jpa

--- spring-data-jpa/src/main/java/com/markruler/datajpa/repository/MemberRepository.java
+++ spring-data-jpa/src/main/java/com/markruler/datajpa/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package com.markruler.datajpa.repository;
 
+import com.markruler.datajpa.dto.MemberDto;
 import com.markruler.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -23,4 +25,17 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
 
+    // @Query는 이름이 없는 NamedQuery처럼 동작한다고 생각할 수 있다.
+    // 컴파일 시 쿼리에 오류가 있다면 QueryException를 던질 수 있다.
+    // 동적 쿼리는 QueryDSL을 사용한다.
+    @Query("select m from Member m where m.username in :usernames and m.age = :age")
+    List<Member> findUser(
+            @Param("usernames") Collection<String> usernames, // Collection Parameter Binding
+            @Param("age") int age
+    );
+
+    // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repository-query-return-types
+    @Query("select new com.markruler.datajpa.dto.MemberDto(m.id, m.username, t.name)" +
+            " from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
--- spring-data-jpa/src/test/java/com/markruler/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/com/markruler/datajpa/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.markruler.datajpa.repository;
 
+import com.markruler.datajpa.dto.MemberDto;
 import com.markruler.datajpa.entity.Member;
+import com.markruler.datajpa.entity.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +23,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     @DisplayName("Repository 프록시 객체")
@@ -88,8 +94,8 @@ class MemberRepositoryTest {
         // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
         List<Member> members = memberRepository.findByUsernameAndAgeGreaterThan("member2", 15);
 
-        assertThat(members.get(0).getUsername()).isEqualTo("member2");
-        assertThat(members.get(0).getAge()).isEqualTo(20);
+        assertThat(members.get(0).getUsername()).isEqualTo(member2.getUsername());
+        assertThat(members.get(0).getAge()).isEqualTo(member2.getAge());
     }
 
     @Test
@@ -100,9 +106,65 @@ class MemberRepositoryTest {
         memberRepository.save(member1);
         memberRepository.save(member2);
 
-        List<Member> members = memberRepository.findByUsername("member2");
+        List<Member> members = memberRepository.findByUsername(member2.getUsername());
 
-        assertThat(members.get(0).getUsername()).isEqualTo("member2");
-        assertThat(members.get(0).getAge()).isEqualTo(20);
+        assertThat(members.get(0).getUsername()).isEqualTo(member2.getUsername());
+        assertThat(members.get(0).getAge()).isEqualTo(member2.getAge());
+    }
+
+    @Test
+    @DisplayName("Query Annotation을 사용해 직접 쿼리를 작성해서 실행할 수 있다")
+    void query_annotation() {
+        Member member1 = new Member("member1", 10);
+        Member member2 = new Member("member2", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        /*
+            select
+                member0_.member_id as member_i1_0_,
+                member0_.age as age2_0_,
+                member0_.team_id as team_id4_0_,
+                member0_.username as username3_0_
+            from
+                member member0_
+            where
+                (
+                    member0_.username in (
+                        ? , ?
+                    )
+                )
+                and member0_.age=?
+         */
+        List<Member> members = memberRepository.findUser(Arrays.asList(member1.getUsername(), member2.getUsername()), member1.getAge());
+
+        assertThat(members.get(0).getUsername()).isEqualTo(member1.getUsername());
+        assertThat(members.get(0).getAge()).isEqualTo(member1.getAge());
+    }
+
+    @Test
+    @DisplayName("Query Annotation을 사용해 DTO를 조회할 수 있다")
+    void query_annotation_find_dto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member member = new Member("member1", 10);
+        member.setTeam(team);
+        memberRepository.save(member);
+
+        /*
+            select
+                member0_.member_id as col_0_0_,
+                member0_.username as col_1_0_,
+                team1_.name as col_2_0_
+            from
+                member member0_
+            inner join
+                team team1_
+                    on member0_.team_id=team1_.team_id
+         */
+        List<MemberDto> members = memberRepository.findMemberDto();
+
+        assertThat(members.get(0).getUsername()).isEqualTo(member.getUsername());
     }
 }
