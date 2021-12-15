c1c478ec5313e8f73b335b469dbf3cb3e998f6c3
Changsu Im <imcxsu@gmail.com>
2021-12-16 02:11:58 +0900

    Test sort - querydsl

--- querydsl/src/main/java/com/markruler/querydsl/entity/Member.java
+++ querydsl/src/main/java/com/markruler/querydsl/entity/Member.java
@@ -33,6 +33,11 @@ public class Member {
     @JoinColumn(name = "team_id")
     private Team team;
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
     public Member(String username, int age, Team team) {
         this.username = username;
         this.age = age;
