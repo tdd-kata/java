becb927e344c5170769a713fc9a7e9954e1ef7e7
Changsu Im <imcxsu@gmail.com>
2021-12-17 00:14:30 +0900

    Test complex predicates - querydsl

--- querydsl/src/test/java/com/markruler/querydsl/entity/Querydsl02AdvancedTest.java
+++ querydsl/src/test/java/com/markruler/querydsl/entity/Querydsl02AdvancedTest.java
@@ -8,7 +8,9 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.AfterEach;
@@ -23,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.markruler.querydsl.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -219,6 +222,9 @@ class Querydsl02AdvancedTest {
 
     }
 
+    /**
+     * @see <a href="https://querydsl.com/static/querydsl/5.0.0/reference/html_single/#d0e2053">Complex predicates - Querydsl Docs</a>
+     */
     @Nested
     @DisplayName("동적 쿼리 생성 시 BooleanBuilder를 사용할 경우")
     class Describe_dynamic_expressions_using_BooleanBuilder {
@@ -267,6 +273,82 @@ class Querydsl02AdvancedTest {
                 final String usernameParam = "member1";
                 final Integer ageParam = null;
 
+                List<Member> members = searchMember(usernameParam, ageParam);
+                assertThat(members).hasSize(1);
+            }
+        }
+    }
+
+    /**
+     * @see <a href="https://querydsl.com/static/querydsl/5.0.0/reference/html_single/#d0e2053">Complex predicates - Querydsl Docs</a>
+     */
+    @Nested
+    @DisplayName("동적 쿼리 생성 시 where 절에 다중 파라미터를 입력할 경우")
+    class Describe_complex_expressions {
+
+        /*
+            - 잘게 쪼개진 메서드들을 다른 쿼리에서도 재활용할 수 있다!!!
+            - 쿼리의 가독성이 높아진다.
+         */
+        private List<Member> searchMember(final String usernameCondition,
+                                          final Integer ageCondition) {
+            return queryFactory
+                    .selectFrom(member)
+                    .where(usernameEq(usernameCondition),
+                            ageEq(ageCondition))
+                    .fetch();
+        }
+
+        private Predicate usernameEq(final String usernameCondition) {
+            return usernameCondition != null ? member.username.eq(usernameCondition) : null;
+        }
+
+        private Predicate ageEq(Integer ageCondition) {
+            return ageCondition != null ? member.age.eq(ageCondition) : null;
+        }
+
+        @Nested
+        @DisplayName("매개 변수가 모두 null이 아니라면")
+        class Context_with_parameters {
+
+            @Test
+            @DisplayName("모든 조건을 검사한다")
+            void sut_query_should_create_where_username_age() {
+                final String usernameParam = "member1";
+                final Integer ageParam = 10;
+
+                /*
+                    select
+                        member1
+                    from
+                        Member member1
+                    where
+                        member1.username = ?1
+                        and member1.age = ?2
+                 */
+                List<Member> members = searchMember(usernameParam, ageParam);
+                assertThat(members).hasSize(1);
+            }
+        }
+
+        @Nested
+        @DisplayName("매개 변수 값이 null이라면")
+        class Context_with_username_condition {
+
+            @Test
+            @DisplayName("해당 조건을 무시한다")
+            void sut_query_should_create_where_username() {
+                final String usernameParam = "member1";
+                final Integer ageParam = null;
+
+                /*
+                    select
+                        member1
+                    from
+                        Member member1
+                    where
+                        member1.username = ?1
+                 */
                 List<Member> members = searchMember(usernameParam, ageParam);
                 assertThat(members).hasSize(1);
             }
