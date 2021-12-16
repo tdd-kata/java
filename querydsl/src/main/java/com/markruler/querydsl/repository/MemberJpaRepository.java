fa0e4b69ba3f341345e9f5699314fa359d902b54
Changsu Im <imcxsu@gmail.com>
2021-12-17 03:00:50 +0900

    Test complex expressions with jpa repository, querydsl

--- querydsl/src/main/java/com/markruler/querydsl/repository/MemberJpaRepository.java
+++ querydsl/src/main/java/com/markruler/querydsl/repository/MemberJpaRepository.java
@@ -1,6 +1,11 @@
 package com.markruler.querydsl.repository;
 
+import com.markruler.querydsl.dto.MemberSearchCondition;
+import com.markruler.querydsl.dto.MemberTeamDto;
+import com.markruler.querydsl.dto.QMemberTeamDto;
 import com.markruler.querydsl.entity.Member;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +14,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.markruler.querydsl.entity.QMember.member;
+import static com.markruler.querydsl.entity.QTeam.team;
+import static org.springframework.util.StringUtils.hasText;
 
 @Repository
 public class MemberJpaRepository {
@@ -54,5 +61,80 @@ public class MemberJpaRepository {
                 .selectFrom(member)
                 .where(member.username.eq(username))
                 .fetch();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName"))
+                )
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search(final MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName"))
+                )
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        // ageGoe(condition.getAgeGoe()),
+                        // ageLoe(condition.getAgeLoe())
+                        ageBetween(condition.getAgeGoe(), condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(final String username) {
+        return username != null ? member.username.eq(username) : null;
+    }
+    private BooleanExpression teamNameEq(final String teamName) {
+        return teamName != null ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(final Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(final Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+
+    // Predicate를 상속한 BooleanExpression을 반환하면 and(), or() 등으로 체이닝할 수 있다.
+    private BooleanExpression ageBetween(final Integer ageGoe, final Integer ageLoe) {
+        if (ageGoe == null || ageLoe == null) {
+            return null;
+        }
+        return ageGoe(ageGoe).and(ageLoe(ageLoe));
     }
 }
