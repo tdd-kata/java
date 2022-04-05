5cdbbad587b71cd292f08d2a311f028ab1d85861
Changsu Im <imcxsu@gmail.com>
2022-04-05 22:15:24 +0900

    Spring Boot Thymeleaf with Vue.js

---
+++ spring-boot-vue/README.md
@@ -0,0 +1,3 @@
+# Spring Boot - Vue.js
+
+- [출처](https://github.com/eugenp/tutorials/blob/master/spring-boot-modules/spring-boot-vue)
---
+++ spring-boot-vue/src/test/java/org/xpdojo/springbootvue/SpringBootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xpdojo.springbootvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
