f6245be45dc3a243a638b2f260b3cd8ad1183a1b
Changsu Im <imcxsu@gmail.com>
2022-04-22 22:49:31 +0900

    Bridge pattern

---
+++ design-patterns/src/main/java/org/xpdojo/designpatterns/_02_structural_patterns/_02_bridge/remote/Remote.java
@@ -0,0 +1,13 @@
+package org.xpdojo.designpatterns._02_structural_patterns._02_bridge.remote;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
