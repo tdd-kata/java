f1cfee778116cb779b4587ab2f11fced01a8c0b7
Changsu Im <imcxsu@gmail.com>
2022-04-23 21:47:34 +0900

    Command pattern

---
+++ design-patterns/src/main/java/org/xpdojo/designpatterns/_03_behavioral_patterns/_02_command/Command.java
@@ -0,0 +1,9 @@
+package org.xpdojo.designpatterns._03_behavioral_patterns._02_command;
+
+public interface Command {
+
+    void execute();
+
+    void undo();
+
+}
---
+++ design-patterns/src/main/java/org/xpdojo/designpatterns/_03_behavioral_patterns/_02_command/LightOffCommand.java
@@ -0,0 +1,20 @@
+package org.xpdojo.designpatterns._03_behavioral_patterns._02_command;
+
+public class LightOffCommand implements Command {
+
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+
+    @Override
+    public void undo() {
+        new LightOnCommand(this.light).execute();
+    }
+}
