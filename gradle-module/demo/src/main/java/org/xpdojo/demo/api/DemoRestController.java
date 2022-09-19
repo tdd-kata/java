148c2aae82d5b05045d866643b07d30e807bd946
Changsu Im <imcxsu@gmail.com>
2022-09-19 16:23:15 +0900

    with gradle, multi module project

---
+++ gradle-module/demo/src/main/java/org/xpdojo/demo/api/DemoRestController.java
@@ -0,0 +1,36 @@
+package org.xpdojo.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xpdojo.aggregate.application.CountOptionsService;
+import org.xpdojo.aggregate.dto.Option;
+import org.xpdojo.search.application.SearchVehicleService;
+import org.xpdojo.search.dto.Product;
+
+import java.util.List;
+
+@RestController
+public class DemoRestController {
+
+    private final SearchVehicleService searchVehicleService;
+    private final CountOptionsService countOptionsService;
+
+    public DemoRestController(
+            SearchVehicleService searchVehicleService,
+            CountOptionsService countOptionsService
+    ) {
+        this.searchVehicleService = searchVehicleService;
+        this.countOptionsService = countOptionsService;
+    }
+
+    @GetMapping("/products")
+    public List<Product> listProducts() {
+        return searchVehicleService.listProducts();
+    }
+
+    @GetMapping("/options")
+    public List<Option> listOptions() {
+        return countOptionsService.listOptions();
+    }
+
+}
