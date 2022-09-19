8a1a84c375d2defc391f8faf2082942405fb6c3c
Changsu Im <imcxsu@gmail.com>
2022-09-19 18:21:10 +0900

    testing gradle multi module project

---
+++ gradle-multi-app/admin/src/test/java/org/xpdojo/admin/AdminTest.java
@@ -0,0 +1,16 @@
+package org.xpdojo.admin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AdminTest {
+
+    @Test
+    void test_admin() {
+        String actual = "admin";
+        String expected = "admin";
+        assertEquals(expected, actual);
+    }
+
+}
