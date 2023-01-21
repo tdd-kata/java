c8f24f940c5fd3f58e49063ef788a40e1400d82d
Changsu Im <imcxsu@gmail.com>
2023-01-22 03:25:24 +0900

    ApplicationContext 주입

--- web-spring-container/src/main/java/org/xpdojo/webspringcontainer/DemoApplication.java
+++ web-spring-container/src/main/java/org/xpdojo/webspringcontainer/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -12,6 +15,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class DemoApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    /**
+     * DispatcherServlet은 ApplicationContextAware의 구현체다.
+     * Spring Container가 DispatcherServlet에게 ApplicationContext를 주입한다.
+     * DispatcherServlet은 ApplicationContext를 통해 Bean을 찾아서 처리한다.
+     *
+     * @see ApplicationContextAware#setApplicationContext(ApplicationContext)
+     */
     public static void main(String[] args) {
         AnnotationConfigServletWebApplicationContext applicationContext =
                 new AnnotationConfigServletWebApplicationContext() {
@@ -19,11 +39,12 @@ public class DemoApplication {
                     protected void onRefresh() {
                         super.onRefresh();
 
-                        ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+                        ServletWebServerFactory factory = this.getBean(ServletWebServerFactory.class);
+                        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                        // dispatcherServlet.setApplicationContext(this);
 
                         WebServer webServer = factory.getWebServer(servletContext -> {
-                            servletContext.addServlet("dispatcherServlet",
-                                            new DispatcherServlet(this)) // require GenericWebApplicationContext
+                            servletContext.addServlet("dispatcherServlet", dispatcherServlet)
                                     .addMapping("/*");
                         });
 
