def02285b7b491fec73c9007b08d181f08f4105d
Changsu Im <imcxsu@gmail.com>
2023-01-22 03:03:41 +0900

    GenericWebApplicationContext -> AnnotationConfigServletWebApplicationContext

--- web-spring-container/src/main/java/org/xpdojo/webspringcontainer/DemoApplication.java
+++ web-spring-container/src/main/java/org/xpdojo/webspringcontainer/DemoApplication.java
@@ -2,32 +2,45 @@ package org.xpdojo.webspringcontainer;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class DemoApplication {
 
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
+        AnnotationConfigServletWebApplicationContext applicationContext =
+                new AnnotationConfigServletWebApplicationContext() {
+                    @Override
+                    protected void onRefresh() {
+                        super.onRefresh();
 
-                ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+                        ServletWebServerFactory factory = new TomcatServletWebServerFactory();
 
-                WebServer webServer = factory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet",
-                                    new DispatcherServlet(this)) // require GenericWebApplicationContext
-                            .addMapping("/*");
-                });
+                        WebServer webServer = factory.getWebServer(servletContext -> {
+                            servletContext.addServlet("dispatcherServlet",
+                                            new DispatcherServlet(this)) // require GenericWebApplicationContext
+                                    .addMapping("/*");
+                        });
 
-                webServer.start();
-            }
-        };
-        // Bean 등록 순서와 상관없이 DI가 가능하다.
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+                        webServer.start();
+                    }
+                };
+
+        applicationContext.register(DemoApplication.class);
         applicationContext.refresh();
     }
 
