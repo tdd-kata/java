47f442eaa417c5bd741c17ad6ee54076b24c0fe5
Changsu Im <imcxsu@gmail.com>
2023-02-20 17:21:10 +0900

    add simple quartz job

---
+++ spring-boot-quartz/src/main/java/org/xpdojo/quartz/QuartzJobListener.java
@@ -0,0 +1,40 @@
+package org.xpdojo.quartz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobListener;
+
+@Slf4j
+public class QuartzJobListener implements JobListener {
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+
+    /**
+     * JobDetail의 execute() 메서드가 실행되기 전에 실행된다.
+     */
+    @Override
+    public void jobToBeExecuted(JobExecutionContext context) {
+        log.info("Job To Be Executed");
+    }
+
+    /**
+     * vetoJobExecution() 메서드가 true를 리턴하면 실행된다.
+     */
+    @Override
+    public void jobExecutionVetoed(JobExecutionContext context) {
+        log.info("Job Execution Vetoed");
+    }
+
+    /**
+     * JobDetail의 execute() 메서드가 실행된 후에 실행된다.
+     */
+    @Override
+    public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
+        log.info("Job was executed");
+    }
+}
